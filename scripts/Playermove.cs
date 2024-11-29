88572221adb0aeb6221996b591ddbbf0f145c6d7
Maarten <92666704+Maarten0162@users.noreply.github.com>
2024-11-29 09:27:18 +0100

    move animation link

--- scripts/Playermove.cs
+++ scripts/Playermove.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 public partial class Playermove : CharacterBody2D
 {
 	Vector2 playerVelocity;
+    AnimatedSprite2D animatedSprite2D;
 	[Export]    int movementSpeed = 500;
 
-	public override async void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+        animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+    }
+
+    public override async void _PhysicsProcess(double delta)
     {
         HandleInput();
         Velocity = playerVelocity;
@@ -20,5 +27,32 @@ public partial class Playermove : CharacterBody2D
     {
         playerVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		playerVelocity = playerVelocity *= movementSpeed;
+        Sprite();
     }
+    private void Sprite()
+	{
+	if(playerVelocity.X < 0)
+		{
+			animatedSprite2D.FlipH = true;
+			animatedSprite2D.Play("move_sideways");
+		}
+		else if(playerVelocity.Y < 0)
+		{
+			animatedSprite2D.FlipH = false;
+			animatedSprite2D.Play("move_up");
+		}	
+		else if(playerVelocity.X > 0)
+		{	
+			animatedSprite2D.FlipH = false;
+			animatedSprite2D.Play("move_sideways");
+		}	
+		else if(playerVelocity.Y > 0)
+		{
+			animatedSprite2D.FlipH = false;
+			animatedSprite2D.Play("move_down");
+		}
+        else animatedSprite2D.Stop();
+		
+	}
+
 }
